15da9844fbb1442b22730d9ffb7fd101df57c711
Ruben Alexandre Silva <66267648+srkzz@users.noreply.github.com>
2023-05-26 11:12:43 +0100

    Add files via upload

---
+++ TaskManager/TaskManager/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Process[] proc;
+
+        private void GetAllProcess()
+        {
+            proc = Process.GetProcesses();
+            listBox.Items.Clear();
+            foreach (Process p in proc)
+                listBox.Items.Add(p.ProcessName);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                proc[listBox.SelectedIndex].Kill();
+                GetAllProcess();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GetAllProcess();
+        }
+
+        private void novaTarefaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (frmRunTask frm = new frmRunTask())
+            {
+                if (frm.ShowDialog() == DialogResult.OK)
+                    GetAllProcess();
+            }
+        }
+    }
+}
---
+++ TaskManager/TaskManager/frmRunTask.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskManager
+{
+    public partial class frmRunTask : Form
+    {
+        public frmRunTask()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtOpen.Text))
+            {
+                try
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = txtOpen.Text;
+                    proc.Start();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
